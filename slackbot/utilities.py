8496c8655706acc8efc347e3f76cdfa028722da1
Raymond DeVries <raymond.l.devries@gmail.com>
2025-12-11 00:07:26 -0800

    Change model and add prints for now

--- slackbot/utilities.py
+++ slackbot/utilities.py
@@ -10,18 +10,20 @@ from slackbot.constants import Classifier, MessageClassified
 
 @cache
 def model() -> Model:
-    model = llm.get_model("openrouter/meta-llama/llama-3.1-8b-instruct")
+    model = llm.get_model("openrouter/qwen/qwen3-8b")
     model.key = os.environ["OPEN_ROUTER_KEY"]
     return model
 
 
 def classify(message: str) -> Classifier:
+    print(f"Classifying message: {message}")
     response = model().prompt(
         message,
         system=f"Classify this slack message. Classification options: ({[x.value for x in Classifier]}).",
         schema=MessageClassified,
     )
     classification = MessageClassified.model_validate_json(response.text()).classified
+    print(classification)
     return classification
 
 
